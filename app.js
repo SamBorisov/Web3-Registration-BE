3a718c8b3a0c49cd26b3f63402da5098c65aea92
SamBorisov <samuil.bo5@gmail.com>
2023-01-23 16:57:41 +0200

    added cors and accept JSON data

--- app.js
+++ app.js
@@ -1,103 +1,114 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 //Setting up server
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200, // For legacy browser support
+    methods: "GET, PUT, POST"
+}
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+
+app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 mongoose.set('strictQuery', false);
 mongoose.connect("mongodb://127.0.0.1:27017/UsersDB", { useNewUrlParser: true })
 
 //schemas
-const userSchema = new mongoose.Schema ({
-    name:  {
+const userSchema = new mongoose.Schema({
+    name: {
         type: String,
         required: [true, "Please provide a your name!"],
-      },
-    email:  {
+    },
+    email: {
         type: String,
         required: [true, "Please provide an Email!"],
         unique: [true, "Email Exist"],
-      },
-    username:  {
+    },
+    username: {
         type: String,
         required: [true, "Please provide a username"],
         unique: [true, "Username Taken"],
-      },
-        address:  {
+    },
+    address: {
         type: String,
         required: [true, "Please provide an address"],
         unique: [true, "Address linked to another account"],
-      },
-  })
+    },
+})
 
 
 const User = mongoose.model("User", userSchema);
 
 //variables
 
-let Logged = false;
+let Logged = true;
 
 const userSample = new User({
     name: "Bob",
     email: "bob1@abv.bg",
     username: "coolbob1",
     address: "hjhd81hd9jd19j1jd1w9jdk0w1dj"
-  })
- // userSample.save();
+})
+// userSample.save();
 
 
-// index page
-app.get('/', function(req, res) {
+//index page
+app.get('/', function (req, res) {
 
-    res.send({ message: "Hello from server!"  + Logged})
-    
+    //res.send({ message: "Hello from server!" + Logged })
+    res.send(Logged)
+
 });
 
 
-// Login & Register
+// Login 
 app.route("/login")
-    .get(function(req, res) {
+    .post((req, res) => {
 
-        User.find({}, (err, result) => {
-           res.send(result);
-        })
-        
-    })
-    .post((req,res)=>{
+        res.set('Access-Control-Allow-Origin', '*');
 
-        console.log(req.body.email)
-
-        nameI = req.body.name;
-        emailI = req.body.email;
         usernameI = req.body.username;
         addressI = req.body.address;
 
-        User.findOne({name: nameI, email: emailI, username: usernameI, address: addressI}, (err, result) => {
+        User.findOne({username: usernameI, address: addressI}, (err, result) => {
             if (result) {
                 console.log("userFound")
-                res.send(result)
                 Logged = true;
+                console.log(Logged)
             } else if (err) {
                 console.log(err)
             } else {
                 res.send("not found")
             }
 
-            })
+        })
+
+        console.log(JSON.stringify(req.body))
     });
 
 
-
+//Register
 app.route("/register")
-    .post((req,res)=>{
+    .get((req,res) => {
+        return Logged;
+    })
+    .post((req, res) => {
 
-        console.log(req.body);
-        console.log(JSON.stringify(req.body));
+        res.set('Access-Control-Allow-Origin', '*');
 
-        nameI = req.body.name;
+
+        nameI =req.body.name;
         emailI = req.body.email;
         usernameI = req.body.username;
         addressI = req.body.address;
@@ -107,15 +118,23 @@ app.route("/register")
             email: emailI,
             username: usernameI,
             address: addressI
-          })
-        //createUser.save();
+        })
+        createUser.save();
 
-        res.redirect('/login')
+        console.log("User created" + JSON.stringify(req.body));
+
     });
 
 
+//Logout
+app.route("/logout")
+    .get((req,res) => {
+
+        Logged = false;
+        console.log(Logged)
+
+    });
 
 
-
-    //Port
-app.listen(3001, () => console.log(`Server is running on port 3001.`));
+//Port
+app.listen(4000, () => console.log(`Server is running on port 4000.`));
